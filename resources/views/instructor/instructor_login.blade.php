5c99333c23bc8fa0406804e70c8eb402b7f788b5
Dwiki Nur Jayanto <dwikinj@gmail.com>
2024-07-18 21:20:38 +0700

    feat: add toastr di instructor_login blade

--- resources/views/instructor/instructor_login.blade.php
+++ resources/views/instructor/instructor_login.blade.php
@@ -20,7 +20,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <link href="{{ asset('backend/assets/css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/assets/css/icons.css') }}" rel="stylesheet">
-    <title>Rocker - Bootstrap 5 Admin Dashboard Template</title>
+    <!-- toastr -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+    <title>Instructor login</title>
 </head>
 
 <body class="">
@@ -163,6 +165,33 @@
     </script>
     <!--app JS-->
     <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+
+    {{-- Toasterjs --}}
+	  
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+	<script>
+	 @if(Session::has('message'))
+	 var type = "{{ Session::get('alert-type','info') }}"
+	 switch(type){
+		case 'info':
+		toastr.info(" {{ Session::get('message') }} ");
+		break;
+	
+		case 'success':
+		toastr.success(" {{ Session::get('message') }} ");
+		break;
+	
+		case 'warning':
+		toastr.warning(" {{ Session::get('message') }} ");
+		break;
+	
+		case 'error':
+		toastr.error(" {{ Session::get('message') }} ");
+		break; 
+	 }
+	 @endif 
+	</script>
 </body>
 
 </html>
