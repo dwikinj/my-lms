4d9bf32425220e8e9c306eb547cc30053c46055c
Dwiki Nur Jayanto <dwikinj@gmail.com>
2024-07-27 08:08:04 +0700

    feat: define discount percentage attr and instructor relationship in course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,6 +10,16 @@ class Course extends Model
     use HasFactory;
     protected $guarded = [];
 
+    public function getDiscountPercentageAttribute()
+    {
+        if ($this->selling_price > 0 && $this->discount_price !== null && $this->discount_price < $this->selling_price) {
+            $discountAmount = $this->selling_price - $this->discount_price;
+            $percentage = ($discountAmount / $this->selling_price) * 100;
+            return round($percentage);
+        }
+        return 0;
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class,'category_id','id');
@@ -22,5 +32,10 @@ class Course extends Model
 
     public function courseSections() {
         return $this->hasMany(CourseSection::class,'course_id','id');
+    }
+
+    public function instructor()
+    {
+        return $this->belongsTo(User::class,'instructor_id','id');
     }
 }
